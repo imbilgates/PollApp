3fdb69b702f70e4a57f50fcab84536eac3fee388
bilgates27 <bilgates270703@gmail.com>
2024-12-24 00:54:53 +0530

    first commit

--- src/main/java/com/bilgates/pollApp/config/WebConfig.java
+++ src/main/java/com/bilgates/pollApp/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")  // Allow any origin (no credentials needed)
+                        .allowedOrigins("http://localhost:3000")  // Allow any origin (no credentials needed)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false); // Disallow credentials (cookies, headers)
