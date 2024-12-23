7e2a84a2e9f0b411d632129613042f0a40fa1cc1
bilgates27 <bilgates270703@gmail.com>
2024-12-24 00:28:31 +0530

    first commit

--- src/main/java/com/bilgates/pollApp/Controller/UserController.java
+++ src/main/java/com/bilgates/pollApp/Controller/UserController.java
@@ -2,12 +2,14 @@ package com.bilgates.pollApp.Controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
 
