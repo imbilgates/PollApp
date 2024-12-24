6fe13168374d97e3ed0556bc35ad723d682bf2e2
bilgates27 <bilgates270703@gmail.com>
2024-12-25 05:05:57 +0530

    test

--- src/main/java/com/bilgates/pollApp/Controller/UserController.java
+++ src/main/java/com/bilgates/pollApp/Controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "${FRONTEND_URL}")
+@CrossOrigin(origins = "${FRONTEND_URL}", allowCredentials = "true")
 public class UserController {
 
 
